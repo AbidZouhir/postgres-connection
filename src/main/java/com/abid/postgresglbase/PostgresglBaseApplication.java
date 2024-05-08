f6312ad97b50d6f46601fbe12694f55164efa3cd
zouhir <zouhir.etransit@gmail.com>
2024-05-08 15:31:03 +0100

    first commit

---
+++ src/main/java/com/abid/postgresglbase/PostgresglBaseApplication.java
@@ -0,0 +1,13 @@
+package com.abid.postgresglbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresglBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresglBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abid/postgresglbase/PostgresglBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abid.postgresglbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresglBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
